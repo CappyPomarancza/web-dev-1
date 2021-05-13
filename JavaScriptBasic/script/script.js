b7f25eb07fbaa60b66bbe37ab4d469f3fa77b034
Kacper Swietlaga <kacper.swietlaga@elcaro.pl>
2021-05-13 18:22:11 +0200

    add math operators

--- JavaScriptBasic/script/script.js
+++ JavaScriptBasic/script/script.js
@@ -51,8 +51,8 @@ let anotherName = '';
 let onlyTrue = true;
 let onlyFalse = false;
 
-console.log('onlyTrue', !onlyTrue);
-console.log('onlyFalse', !onlyFalse);
+// console.log('onlyTrue', !onlyTrue);
+// console.log('onlyFalse', !onlyFalse);
 //
 // console.log('onlyFalse', onlyFalse);
 // console.log('onlyFalse', typeof onlyFalse);
@@ -70,3 +70,87 @@ console.log('onlyFalse', !onlyFalse);
 // console.log('name', !!name);
 // console.log('fuel', !!fuel);
 // console.log('anotherName', !!anotherName);
+
+let x = 1 + 2;
+// console.log('x', x);
+let y = 1 - 2;
+// console.log('y', y);
+let z = -0;
+let p = 0;
+// console.log('z', z);
+// console.log('z', typeof z);
+// console.log('c', typeof c);
+let infinity = -Infinity;
+// console.log('infinity', infinity);
+
+let helloLabel = 'Hello ' + name + '!';
+// console.log('helloLabel', helloLabel);
+
+let a = 290;
+let b = 4;
+
+// a+=a; // przypisanie do wartości a jej obecnej wartości i dodanie wartości zmiennej a
+// a = a + a;
+// a+=5; // przypisanie do wartości a jej obecnej wartości i dodanie wartości 5;
+// a = a + 5;
+// console.log('wartość zmiannej a:', a);
+
+// console.log('a--', a--);
+// a++; // zwiększenie o 1 wartości przypisanej do zmiennej a
+// console.log('a', a);
+
+//
+// console.log('a', a);
+//
+//
+// console.log('--a', --a);
+//
+//
+// console.log('a', a);
+//
+// console.log('b+=b', b += b);
+//
+// console.log('b++', b++);
+//
+// console.log('b', b);
+
+
+// console.log('++b', ++b);
+
+// console.log('b', b);
+
+//Equality ==
+
+// console.log('1 == 1', 1 == 1);
+
+// console.log("1 == '1'", 1 == '1');
+// console.log("1 != '1'", 1 != '1');
+
+// console.log("'1' == 1", '1' == 1);
+// console.log("'1' == 1", '1' == 1);
+
+// console.log("!!0", !!0);
+// console.log("0 == false", 0 == false);
+// console.log("'a' == 'A'", 'a' == 'A');
+
+let object1 = {key: 'value'};
+let object2 = {key: 'value'};
+let object3 = object2;
+// console.log('object1 == object2', object1 == object2);
+// console.log('object2 == object3', object2 == object3);
+
+//Strict Equality ===
+// console.log("1 === 1", 1 === 1);
+// console.log("1 === '1'", 1 === '1');
+// console.log("0 === false", 0 === false);
+
+
+// console.log("1 !== 1", 1 !== 1);
+// console.log("1 !== '1'", 1 !== '1');
+// console.log("0 === false", 0 === false);
+
+
+console.log("1 >= 2", 1 >= 2);
+console.log("2 >= 2", 2 >= 2);
+
+console.log("1 <= 2", 1 <= 2);
