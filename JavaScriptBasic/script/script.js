98b1969d2bf9f83f6bc4f97bd16f6bbf635344bb
Kamila Makarewicz <makarewicz.kamila@gmail.com>
2021-05-20 17:18:57 +0200

    my changes

---
+++ JavaScriptBasic/script/script.js
@@ -0,0 +1,19 @@
+let age = 90;
+let name = 'Kamila';
+const lastName = 'Makarewicz';
+const multipleTypeArray = ['Kamila', 30, lastName, name]
+
+console.log('age', typeof age);
+console.log('name', typeof name);
+console.log('multipleTypeArray', typeof multipleTypeArray);
+
+const person = {
+    name: 'Kamila',
+    lastName: 'Makarewicz',
+    age: 29,
+    email: 'makarewicz.kamila@gmail.com'
+
+};
+
+console.log('person', typeof person)
+console.log('person', person)
